51b6dd74a9c0c543dff8551df41589984374c838
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-11 09:37:46 -0700

    add total_salary

--- lib/show.rb
+++ lib/show.rb
@@ -1,11 +1,15 @@
 class Show
 
-attr_reader :name, :creator, :characters
+  attr_reader :name, :creator, :characters, :total_salary
 
-def initialize(name, creator, characters)
-  @name = name
-  @creator = creator
-  @characters = characters
-end
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    characters.map { |character| character.salary }.sum
+  end
 
 end

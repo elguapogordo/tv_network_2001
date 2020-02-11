82c2361b795ffae6be8a77fa7c3e962e2a78b64a
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-11 09:18:33 -0700

    add character initialize

--- lib/character.rb
+++ lib/character.rb
@@ -1,1 +1,11 @@
+class Character
+  attr_reader :details, :name, :actor, :salary
 
+  def initialize(details)
+    @details = details
+    @name = details[:name]
+    @actor = details[:actor]
+    @salary = details[:salary]
+  end
+
+end

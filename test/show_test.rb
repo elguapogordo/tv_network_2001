bb1b054477412d98de065c9e724b420f28e4558b
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-11 09:37:28 -0700

    add total salary test

--- test/show_test.rb
+++ test/show_test.rb
@@ -14,4 +14,12 @@ def test_it_exists
   assert_instance_of Show, knight_rider
 end
 
+def test_it_can_give_total_salary
+  kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+  knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+  assert_equal 2600000, knight_rider.total_salary
+end
+
 end

723016066454e09874e148c508a0b83c845e62ef
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-11 11:01:17 -0700

    add actors by show

--- lib/network.rb
+++ lib/network.rb
@@ -1,6 +1,6 @@
 class Network
 
-  attr_reader :name, :shows
+  attr_reader :name, :shows, :actors_by_show
 
   def initialize(name)
     @name = name
@@ -19,6 +19,13 @@ class Network
       end
     end
     characters
+  end
+
+  def actors_by_show
+    @actors_by_show = Hash.new { |hash, key| hash[key] = [] }
+    shows.each do |show|
+      @actors_by_show[show.name] = show.actors
+    end
   end
 
 end
